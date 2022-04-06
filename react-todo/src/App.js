18b11f4847b39d7d4637c3daef99944f8a7c21e6
show kawazoe <show753@icloud.com>
2022-04-06 13:22:35 +0900

    Set useEffect

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShoeFlag ] = useState(false);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShoeFlag = () => {
+    setFaceShoeFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShoeFlag(true);
+      } else {
+        faceShowFlag && setFaceShoeFlag(false);
+      }
+    }
+  }, [num])
   return (
     <React.Fragment>
       <h1 style={{ color: 'red' }}>こんにちは！</h1>
-      <ColorfulMessage color="blue" message="お元気ですか？" />
-      <ColorfulMessage color="pink" message="元気です！" />
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShoeFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>（＾ω＾）</p>}
     </React.Fragment>
 
   );
--- react-todo/src/components/ColorfulMessage.jsx
+++ react-todo/src/components/ColorfulMessage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const ColorfulMessage = (props) => {
-  console.log(props);
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: '18px'
   };
 
   return (
-    <p style={contentStyle}>{props.message}</p>
+    <p style={contentStyle}>{children}</p>
   );
 };
 
